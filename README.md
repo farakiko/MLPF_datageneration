bd22a1a2cfbaa886ec6a56daca6168113f444948
Farouk <farouk.mokhtar@gmail.com>
2025-02-21 15:16:39 +0100

    up readme and sim script

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ In order to use the repo:
 `git clone https://github.com/key4hep/CLDConfig/tree/main/CLDConfig`
 update the path in `scripts/run_sequence_CLD_train.sh`. 
 
-The script `script_create_dataset_train.sh` can be used to create condor jobs, by default for the gun there are 100 events, configure in the config. 
+The script `script_create_dataset_train_gun.sh` can be used to create condor jobs, by default for the gun there are 100 events, configure in the config. 
 The gun config in `gun/config_files/*.gun` includes:
 `npart_range 10,15 %range of number of particles`
 `eta_range -0.5,0.5   %eta range`
@@ -10,3 +10,5 @@ The gun config in `gun/config_files/*.gun` includes:
 `drmax 0.5  %if dr max is larger than 0 it generates particles with sqrt((eta)**2+(phi)**2)<drmax, if it's 0 then it create random angles `
 `pid_list 211,-211,130,130,2112,2112,-2112,-2112,22,22,22,22,22,22,11,-11,11,-11,11,-11,13,-13,13,-13,13,-13,-321,321,2212,-2212 %pdg of particles (they need to be available in the gun definition otherwise the mass is 0)`
 `nevents 100`
+
+The script `script_create_dataset_train_ttbar.sh` can be used to create condor jobs for `p8_ee_tt_ecm365` events, by default there are 100 events, configure in the config. 
--- scripts/run_sequence_CLD_train.sh
+++ scripts/run_sequence_CLD_train.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-HOMEDIR=${1} # path to where it's ran from
+HOMEDIR=${1} # path to where it's ran from e.g. /afs/cern.ch/user/f/fmokhtar/MLPF_datageneration
 GUNCARD=${2}  
 NEV=${3}
 SEED=${4}
 OUTPUTDIR=${5}
 DIR=${6}
+SAMPLE=${7} # can be "gun" or "p8_ee_tt_ecm365"
 
-mkdir ${DIR}
-mkdir ${DIR}/${SEED}
+mkdir -p ${DIR}/${SEED}
 cd ${DIR}/${SEED}
 
-SAMPLE="gun"
 
-PATH_CLDCONFIG=/afs/cern.ch/work/m/mgarciam/private/CLD_Config_versions/CLDConfig_030225/CLDConfig/ 
+# PATH_CLDCONFIG=/afs/cern.ch/work/m/mgarciam/private/CLD_Config_versions/CLDConfig_030225/CLDConfig/ 
+PATH_CLDCONFIG=/afs/cern.ch/user/f/fmokhtar/MLPF_datageneration/CLDConfig/CLDConfig
 
 
 wrapperfunction() {
@@ -37,22 +37,23 @@ then
     ./build/gun ${PATH_GUN_CONFIG} 
 fi
 
-if [[ "${SAMPLE}" == "Zcard" ]]
+if [[ "${SAMPLE}" == "p8_ee_tt_ecm365" ]]
 then
-    cp ${HOMEDIR}/Pythia_generation/${SAMPLE}.cmd card.cmd
+    xrdcp ${HOMEDIR}/pythia/${SAMPLE}.cmd card.cmd
     echo "Random:seed=${SEED}" >> card.cmd
     cat card.cmd
-    k4run pythia.py -n $NEV --Dumper.Filename out.hepmc --Pythia8.PythiaInterface.pythiacard card.cmd
+    k4run ${HOMEDIR}/pythia/pythia.py -n $NEV --Dumper.Filename out.hepmc --Pythia8.PythiaInterface.pythiacard card.cmd
     cp out.hepmc events.hepmc
 fi
 
 
 ddsim --compactFile $K4GEO/FCCee/CLD/compact/CLD_o2_v06/CLD_o2_v06.xml --outputFile out_sim_edm4hep.root --steeringFile ${PATH_CLDCONFIG}/cld_steer.py --inputFiles events.hepmc --numberOfEvents ${NEV} --random.seed ${SEED}
 
-cp -r /afs/cern.ch/work/m/mgarciam/private/CLD_Config_versions/CLDConfig_030225/CLDConfig/* .
-k4run CLDReconstruction.py -n ${NEV}  --inputFiles out_sim_edm4hep.root --outputBasename out_reco_edm4hep
+# copy large input files via xrootd (recommended)
+xrdcp -r ${PATH_CLDCONFIG}/* .
+k4run CLDReconstruction.py -n ${NEV}  --inputFiles out_sim_edm4hep.root --outputBasename out_reco
 
 
 
 mkdir -p ${OUTPUTDIR}
-python /afs/cern.ch/work/f/fccsw/public/FCCutils/eoscopy.py out_sim_edm4hep.root ${OUTPUTDIR}/out_sim_edm4hep_${SEED}.root
+xrdcp ${DIR}/${SEED}/out_reco_REC.edm4hep.root ${OUTPUTDIR}/reco_${SAMPLE}_${SEED}.root
